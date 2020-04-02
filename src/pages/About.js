b361a3afdead17683416cb2ccbebb1f292a6e0d9
hyojelee <hyojelee1004ss@gmail.com>
2020-04-02 01:23:23 -0400

    almost done

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah
+      </p>
+    </section>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import SearchForm from "../components/SearchForm";
+import CocktailList from "../components/CocktailList";
+
+export default function Home() {
+  const [loading, setLoading] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState("d");
+  const [cocktail, setCocktail] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          const newCocktail = drinks.map(item => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlchoholic,
+              strGlass
+            } = item;
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlchoholic,
+              glass: strGlass
+            };
+          });
+          setCocktail(newCocktail);
+        } else {
+          setCocktail([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getDrinks();
+  }, [searchTerm]);
+
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm}></SearchForm>
+      <CocktailList loading={loading} cocktail={cocktail}></CocktailList>
+    </main>
+  );
+}
